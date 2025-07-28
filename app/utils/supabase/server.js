b17ae5a6fbfb202ad1b216d12c04772ed226d211
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-07-28 13:06:28 +0000

    migrating to js from ts

---
+++ app/utils/supabase/server.js
@@ -0,0 +1,29 @@
+
+import { createServerClient, serialize, parse } from "@supabase/ssr";
+
+export function createClient(request: Request) {
+  const cookies = parse(request.headers.get("Cookie") ?? "");
+  const headers = new Headers();
+
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+  return createServerClient(
+    supabaseUrl!,
+    supabaseKey!,
+    {
+      cookies: {
+        get(key) {
+          return cookies[key];
+        },
+        set(key, value, options) {
+          headers.append("Set-Cookie", serialize(key, value, options));
+        },
+        remove(key, options) {
+          headers.append("Set-Cookie", serialize(key, "", options));
+        },
+      },
+    },
+  );
+}
+
