e5be965356a49d4c49fbbc39807f81e4716d77c2
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-07-29 15:31:08 +0000

    updating supabase import inSessions

--- app/lib/supabaseSessionStorage.js
+++ app/lib/supabaseSessionStorage.js
@@ -5,7 +5,7 @@ const { Session } = sessionStoragePkg;
 
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
+  process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
