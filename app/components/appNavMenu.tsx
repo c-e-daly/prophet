cd9dfcc719c247e4e045963509ab279fbde6cd02
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-08-04 13:31:04 +0000

    updating navemenu

--- app/components/appNavMenu.tsx
+++ app/components/appNavMenu.tsx
@@ -5,8 +5,8 @@ export default function AppNavMenu() {
   return (
     <NavMenu>
       <Link to="/app" rel="home">Home</Link>
-      <Link to="../routes/app.dashboard.tsx">Dashboard</Link>
-      <Link to="../routes/app.portfolios.tsx">Portfolios</Link>
+      <Link to="app/dashbaord">Dashboard</Link>
+      <Link to="app/portfolios">Portfolios</Link>
     </NavMenu>
   );
 }
