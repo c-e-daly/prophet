01a90d9476db0c75e5b394458b9529103b6c2050
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-08-11 20:49:45 +0000

    testing

--- app/routes/app.portfolios.tsx
+++ app/routes/app.portfolios.tsx
@@ -1,12 +1,27 @@
-import { Page, Layout } from '@shopify/polaris';
+import { Page, Layout, Card, Text} from '@shopify/polaris';
 import { Outlet } from "@remix-run/react";
 
 export default function Portfolios() {
   return (
     <Page>
-      <Layout>
+        <Layout> 
+          <Layout.Section variant="oneThird">
+            <Card>
+              <Text as="h3">New Portfolio</Text>
+            </Card>
+          </Layout.Section>
+          <Layout.Section variant="oneThird">
+             <Card>
+              <Text as="h3">Growth Portfolio</Text>
+             </Card>
+          </Layout.Section>
+          <Layout.Section variant="oneThird">
+            <Card>
+                <Text as="h3">Stable Portfolio</Text>
+            </Card>
+          </Layout.Section>
+        </Layout>
         <Outlet />           
-      </Layout>
     </Page>
   );
 }
