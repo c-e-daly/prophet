6897f5a4b46295a22a6984a43cc91cc2bc4a620c
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-08-11 21:00:46 +0000

    setting the index page and subpage content

--- app/routes/app.portfolios._index.tsx
+++ app/routes/app.portfolios._index.tsx
@@ -1,5 +1,40 @@
-import { Text } from '@shopify/polaris';
+import { Page, Layout, Card, Text} from '@shopify/polaris';
 
 export default function PortfoliosIndex() {
-  return
+  return (
+    <Page>
+    <Layout> 
+   <Layout.Section variant="oneThird">
+     <Card>
+       <Text as="h3">New Portfolio</Text>
+     </Card>
+   </Layout.Section>
+   <Layout.Section variant="oneThird">
+      <Card>
+       <Text as="h3">Growth Portfolio</Text>
+      </Card>
+   </Layout.Section>
+   <Layout.Section variant="oneThird">
+     <Card>
+         <Text as="h3">Stable Portfolio</Text>
+     </Card>
+   </Layout.Section>
+    <Layout.Section variant="oneThird">
+      <Card>
+       <Text as="h3">Reactivated Portfolio</Text>
+      </Card>
+   </Layout.Section>
+  <Layout.Section variant="oneThird">
+     <Card>
+       <Text as="h3">Declining Portfolio</Text>
+     </Card>
+    </Layout.Section>
+ <Layout.Section variant="oneThird">
+     <Card>
+     <Text as="h3">Defected Portfolio</Text>
+    </Card>
+  </Layout.Section>
+ </Layout>
+ </Page>
+ )
 }
--- app/routes/app.portfolios.tsx
+++ app/routes/app.portfolios.tsx
@@ -1,41 +1,9 @@
-import { Page, Layout, Card, Text} from '@shopify/polaris';
+import { Page} from '@shopify/polaris';
 import { Outlet } from "@remix-run/react";
 
 export default function Portfolios() {
   return (
     <Page>
-        <Layout> 
-          <Layout.Section variant="oneThird">
-            <Card>
-              <Text as="h3">New Portfolio</Text>
-            </Card>
-          </Layout.Section>
-          <Layout.Section variant="oneThird">
-             <Card>
-              <Text as="h3">Growth Portfolio</Text>
-             </Card>
-          </Layout.Section>
-          <Layout.Section variant="oneThird">
-            <Card>
-                <Text as="h3">Stable Portfolio</Text>
-            </Card>
-          </Layout.Section>
-           <Layout.Section variant="oneThird">
-             <Card>
-              <Text as="h3">Reactivated Portfolio</Text>
-             </Card>
-          </Layout.Section>
-         <Layout.Section variant="oneThird">
-            <Card>
-              <Text as="h3">Declining Portfolio</Text>
-            </Card>
-           </Layout.Section>
-        <Layout.Section variant="oneThird">
-            <Card>
-            <Text as="h3">Defected Portfolio</Text>
-           </Card>
-         </Layout.Section>
-        </Layout>
         <Outlet />           
     </Page>
   );
