22ecf4427128ba407a3fef6958a46ac32a3e1ebc
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-08-11 20:30:02 +0000

    add index pages for nested routes to have a separate home page for the route

---
+++ app/routes/app.portfolios._index.tsx
@@ -0,0 +1,5 @@
+import { Text } from '@shopify/polaris';
+
+export default function PortfoliosIndex() {
+  return <Text as="h1">Customer Portfolios</Text>;
+}
--- app/routes/app.portfolios.tsx
+++ app/routes/app.portfolios.tsx
@@ -1,17 +1,12 @@
-import { Page, Layout, Card, Text, InlineStack, BlockStack, InlineGrid, Divider, Badge, Box} from '@shopify/polaris';
-
+import { Page, Layout } from '@shopify/polaris';
 import { Outlet } from "@remix-run/react";
 
-
 export default function Portfolios() {
-
   return (
     <Page>
-        <Layout>
-            <Text as="h1">Customer Portfolios</Text>
-            <Outlet />           
-        </Layout>
+      <Layout>
+        <Outlet />           
+      </Layout>
     </Page>
-  
   );
 }
