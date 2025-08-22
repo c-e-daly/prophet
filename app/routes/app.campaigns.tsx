aa71ab516774f8cce635a83bf83f5189f588c497
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-08-22 20:11:27 +0000

    trying useloaction in outlet to apply to children

--- app/routes/app.campaigns.tsx
+++ app/routes/app.campaigns.tsx
@@ -1,14 +1,13 @@
-import { Outlet, useSearchParams } from "@remix-run/react";
+import { Outlet, useLocation } from "@remix-run/react";
 import { Page, Button } from "@shopify/polaris";
 
 export default function Campaigns() {
-  const [sp] = useSearchParams();
-  const qs = sp.toString();
-  const toCreate = qs ? `create?${qs}` : "create"; // keep shop/host
+
+  const location = useLocation();
 
   return (
     <Page>
-        <Outlet />
+        <Outlet key={location.pathname}/>
     </Page>
   );
 }
