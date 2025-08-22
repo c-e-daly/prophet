db160aab8faae00baadd1e5eeaf236d0a1e52c77
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-08-22 19:39:55 +0000

    correcting index pages for nested routes

---
+++ app/routes/app.offers._index.tsx
@@ -0,0 +1,17 @@
+import { Page, Layout, Card, Text, InlineStack, BlockStack, InlineGrid, Divider, Badge, Box} from '@shopify/polaris';
+import { BarChart, LineChart, PieChart, ResponsiveContainer, XAxis, YAxis, Tooltip, Legend, CartesianGrid, Bar, Line, Pie, Cell} from 
+'recharts';
+
+
+export default function OffersIndex() {
+
+  return (
+    <Page>
+        <Layout>
+            <Text as="h1">Customer Offers</Text>
+            <Card></Card>
+        </Layout>
+    </Page>
+
+  );
+}
--- app/routes/app.offers.tsx
+++ app/routes/app.offers.tsx
@@ -1,15 +1,11 @@
-import { Page, Layout, Card, Text, InlineStack, BlockStack, InlineGrid, Divider, Badge, Box} from '@shopify/polaris';
-import { BarChart, LineChart, PieChart, ResponsiveContainer, XAxis, YAxis, Tooltip, Legend, CartesianGrid, Bar, Line, Pie, Cell} from 'recharts';
-
+import { Page} from '@shopify/polaris';
+import { Outlet } from "@remix-run/react";
 
 export default function Offers() {
 
   return (
     <Page>
-        <Layout>
-            <Text as="h1">Customer Offers</Text>
-            <Card></Card>
-        </Layout>
+      <Outlet />
     </Page>
 
   );
