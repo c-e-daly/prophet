5230c51bd0283316803d9dc3a6307c6b9d34ccfe
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-08-22 21:39:37 +0000

    bare minimum portolios root

--- app/routes/app.portfolios.tsx
+++ app/routes/app.portfolios.tsx
@@ -1,15 +1,10 @@
 import { Outlet } from "@remix-run/react";
-
-/** Keep parent as a pure layout so children fully control the page. */
-export default function PortfoliosRoot() {
-  return <Outlet />;
-}
-/*
+import {Page, Layout,Card,Text,Button,BlockStack,InlineStack,Box,Badge,Divider} from "@shopify/polaris";
 export default function Portfolios() {
+ 
   return (
     <Page>
         <Outlet />           
     </Page>
   );
-}*/
-
+}
