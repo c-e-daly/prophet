517767a0b1307ff1bda2bb020c1d5f8d02dba5db
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-09-04 15:32:20 +0000

    updating root.tsx to a dumb route

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,28 @@
 // app/root.tsx
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <link rel="stylesheet" href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+/*
+// app/root.tsx
 import {Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData} from "@remix-run/react";
 import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import { getFlexibleShopSession, requirePartialShopSession, 
@@ -51,3 +75,4 @@ export default function App() {
     </html>
   );
 }
+  */
