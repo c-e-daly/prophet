8eb6ffd8d27aa02fc8cb5cec5179f28c4edd340c
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-09-13 16:45:30 +0000

    correcting session inmport on templates.tsx

--- app/routes/app.templates.tsx
+++ app/routes/app.templates.tsx
@@ -2,26 +2,24 @@
 import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { Page } from "@shopify/polaris";
-import { ShopSessionProvider } from "../context/shopSession";
-import { requireShopSession } from "../lib/session/shopAuth.server";
+import { authenticate } from "../shopify.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  const { shopSession, headers } = await requireShopSession(request);
+  const { session } = await authenticate.admin(request);
+  
   return json(
     {
       apiKey: process.env.SHOPIFY_CLIENT_ID || "",
-      shopSession,
-    } as const,
-    { headers }
+      session
+    }
   );
 }
-export default function Templates() {
-  const { shopSession } = useLoaderData<typeof loader>();
+export default function Subscription() {
+  const { session } = useLoaderData<typeof loader>();
   return (
 
-    <ShopSessionProvider value={shopSession}>
     <Page title="Product Template Deployment">
     </Page>
-    </ShopSessionProvider>
+ 
   );
 }
