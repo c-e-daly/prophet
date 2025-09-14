f55799ee9a7654a9fe834ec7ed6a1c2fcbe12ebf
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-09-14 13:05:46 +0000

    reverting to href links

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -5,7 +5,7 @@ import { AppProvider } from "@shopify/shopify-app-remix/react";
 import { NavMenu } from "@shopify/app-bridge-react";
 import { authenticate } from "../shopify.server";
 import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
-import { ShopifyLink } from "../utils/ShopifyLink";
+// import { ShopifyLink } from "../utils/ShopifyLink";
 
 export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
@@ -24,17 +24,38 @@ export default function AppLayout() {
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
       <NavMenu>
-        <ShopifyLink to="/app" rel="home">Home</ShopifyLink>
-        <ShopifyLink to="/app/dashboard">Dashboard</ShopifyLink>
-        <ShopifyLink to="/app/portfolios">Portfolios</ShopifyLink>
-        <ShopifyLink to="/app/offers">Customer Offers</ShopifyLink>
-        <ShopifyLink to="/app/carts">Customer Carts</ShopifyLink>
-        <ShopifyLink to="/app/campaigns">Campaigns</ShopifyLink>
-        <ShopifyLink to="/app/templates">Templates</ShopifyLink>
-        <ShopifyLink to="/app/pricebuilder">Price Builder</ShopifyLink>
-        <ShopifyLink to="/app/subscription">Subscription</ShopifyLink>
+        <a href="/app" rel="home">Home</a>
+        <a href="/app/dashboard">Dashboard</a>
+        <a href="/app/portfolios">Portfolios</a>
+        <a href="/app/offers">Customer Offers</a>
+        <a href="/app/carts">Customer Carts</a>
+        <a href="/app/campaigns">Campaigns</a>
+        <a href="/app/templates">Templates</a>
+        <a href="/app/pricebuilder">Templates</a>
+        <a href="/app/subscription">Subscription</a>
       </NavMenu>
      <Outlet />
     </AppProvider>
   )
 }
+
+
+
+
+
+
+/*
+
+
+
+        <ShopifyLink to="/app" rel="home">Home</ShopifyLink>
+<ShopifyLink to="/app/dashboard">Dashboard</ShopifyLink>
+<ShopifyLink to="/app/portfolios">Portfolios</ShopifyLink>
+<ShopifyLink to="/app/offers">Customer Offers</ShopifyLink>
+<ShopifyLink to="/app/carts">Customer Carts</ShopifyLink>
+<ShopifyLink to="/app/campaigns">Campaigns</ShopifyLink>
+<ShopifyLink to="/app/templates">Templates</ShopifyLink>
+<ShopifyLink to="/app/pricebuilder">Price Builder</ShopifyLink>
+<ShopifyLink to="/app/subscription">Subscription</ShopifyLink>
+
+*/
