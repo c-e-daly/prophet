888f03f568c2e7965d34d69eda2c7480a3a01785
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-10-06 19:17:55 +0000

    updating query to get the counteroffer for the offer by offersID

--- app/lib/queries/supabase/getShopCountersByOffer.ts
+++ app/lib/queries/supabase/getShopCountersByOffer.ts
@@ -1,14 +1,14 @@
 // app/lib/queries/supabase/counterOffers/getCounterOffersForOffer.ts
 import createClient from "../../../../supabase/server";
 
-export async function getCounterOffersForOffer(shopsID: number, offerId: number) {
+export async function getCounterOffersForOffer(shopsID: number, offersID: number) {
   const supabase = createClient();
   
   const { data, error } = await supabase
     .from('counterOffers')
     .select('*')
     .eq('shops', shopsID)
-    .eq('offers', offerId)
+    .eq('offers', offersID)
     .order('createDate', { ascending: false });
   
   if (error) throw error;
