4dec64832255412fa46d5f3dbfcae1aedb111975
Chris D <127351760+c-e-daly@users.noreply.github.com>
2025-10-15 19:56:20 +0000

    updating webhook checkouts update

--- app/routes/webhooks.checkouts-update.tsx
+++ app/routes/webhooks.checkouts-update.tsx
@@ -1,10 +1,16 @@
+// app/routes/webhooks.checkouts.create.ts
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
-import { writeCheckout } from "../lib/webhooks/write";
+import { upsertShopifyCheckoutWebhook } from "../lib/webhooks/upsertShopifyCheckoutWebhook";
+import { getAuthContext } from "../lib/auth/getAuthContext.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
+  if (request.method !== "POST") return new Response("Method Not Allowed", { status: 405 });
+
   const { topic, shop, payload } = await authenticate.webhook(request);
   if (topic !== "CHECKOUTS_UPDATE") return new Response("Wrong topic", { status: 400 });
-  await writeCheckout(payload, shop); // upsert on same table
+  const { shopsID} = await getAuthContext(request);
+
+  await upsertShopifyCheckoutWebhook({ shopDomain: shop, shopsID: shopsID, payload }); // throws on failure
   return new Response("OK");
 };
